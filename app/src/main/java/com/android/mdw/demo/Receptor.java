a73554513d9c500943350524a6d1d7df6d57f637
Xavier <xaavier@protonmail.com>
2020-03-21 10:32:35 +0100

    Added functionalities to receptor class

--- app/src/main/java/com/android/mdw/demo/Receptor.java
+++ app/src/main/java/com/android/mdw/demo/Receptor.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Receptor extends BroadcastReceiver {
 
@@ -16,12 +17,18 @@ public class Receptor extends BroadcastReceiver {
 
         if(action.equals(context.getString(R.string.intentSonido))) {
             //Llamar al servicio
+            Toast.makeText(context, R.string.serviceSonido, Toast.LENGTH_LONG).show();
+            context.startService(intent);
         }
         if(action.equals(context.getString(R.string.intentCancion))) {
             //Llamar al servicio
+            Toast.makeText(context, R.string.serviceCancion, Toast.LENGTH_LONG).show();
+            context.startService(intent);
         }
         if(action.equals(context.getString(R.string.intentDetener))) {
             //Llamar al servicio
+            Toast.makeText(context, R.string.serviceDetener, Toast.LENGTH_LONG).show();
+            context.startService(intent);
         }
     }
 }
