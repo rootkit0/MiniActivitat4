04d3fde2053ea57d4d5492e4bc788f0971debb92
Xavier <xaavier@protonmail.com>
2020-03-25 20:34:31 +0100

    Receptor event when headsets plugged

--- app/src/main/java/com/android/mdw/demo/Receptor.java
+++ app/src/main/java/com/android/mdw/demo/Receptor.java
@@ -15,20 +15,35 @@ public class Receptor extends BroadcastReceiver {
         Bundle data = intent.getExtras();
         String action = data.getString("action");
 
-        if(action.equals(context.getString(R.string.intentSonido))) {
-            //Llamar al servicio
-            Toast.makeText(context, R.string.serviceSonido, Toast.LENGTH_LONG).show();
-            context.startService(intent);
+        if(intent.getAction() == null) {
+            if(action.equals(context.getString(R.string.intentSonido))) {
+                //Llamar al servicio
+                Toast.makeText(context, R.string.serviceSonido, Toast.LENGTH_LONG).show();
+                context.startService(intent);
+            }
+            if(action.equals(context.getString(R.string.intentCancion))) {
+                //Llamar al servicio
+                Toast.makeText(context, R.string.serviceCancion, Toast.LENGTH_LONG).show();
+                context.startService(intent);
+            }
+            if(action.equals(context.getString(R.string.intentDetener))) {
+                //Llamar al servicio
+                Toast.makeText(context, R.string.serviceDetener, Toast.LENGTH_LONG).show();
+                context.startService(intent);
+            }
         }
-        if(action.equals(context.getString(R.string.intentCancion))) {
-            //Llamar al servicio
-            Toast.makeText(context, R.string.serviceCancion, Toast.LENGTH_LONG).show();
-            context.startService(intent);
-        }
-        if(action.equals(context.getString(R.string.intentDetener))) {
-            //Llamar al servicio
-            Toast.makeText(context, R.string.serviceDetener, Toast.LENGTH_LONG).show();
-            context.startService(intent);
+        if(intent.getAction().equals("android.intent.action.HEADSET_PLUG")) {
+            //Cuando se conectan los auriculares
+            if (intent.getIntExtra("state", 0) == 1){
+                Toast.makeText(context, R.string.auxOn, Toast.LENGTH_LONG).show();
+                intent.putExtra("action", context.getString(R.string.intentCancion));
+                context.startService(intent);
+            }
+            //Cuando se desconectan los auriculares
+            else {
+                Toast.makeText(context, R.string.auxOff, Toast.LENGTH_LONG).show();
+                context.stopService(intent);
+            }
         }
     }
 }
